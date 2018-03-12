3dcddd1499c8e64fc664ef548f19e7d5d15fef1e
David Trinh <david_trinh@live.com>
2018-03-11 20:17:50 -0700

    FlashMemV1.05
    Changed to PHP to be uploaded onto AWS.

--- js/flashcardFlip.js
+++ js/flashcardFlip.js
@@ -1,66 +1,57 @@
-var topic = sessionStorage.getItem('topic');
-var subtopic = sessionStorage.getItem('subtopic');
-var i = 1;
+var strPath = sessionStorage.getItem('path'); // Initialize location string for firebase database reference from previous webpage
+var intIndex = 1; // Initialize card index count
+var intLastIndex = 1; // Initialize end of card index
 
+// Initialize card data onto webpage
 window.onload = function() 
 {
 	setCard();
 	document.getElementById("prevBtn").setAttribute("disabled", "disabled");
 }
 
+// Go back one card
 function prev() 
 {
-	i -= 1 ;
-	clearCard();
-	setCard();
-	document.getElementById("nextBtn").removeAttribute("disabled");
-	if (i <= 1)
-	{
-		document.getElementById("prevBtn").setAttribute("disabled", "disabled");
-	}
-}
+	intIndex--;
 
-function next()
-{
-	i += 1;
-	clearCard();
-	setCard();
-	document.getElementById("prevBtn").removeAttribute("disabled");
-	if (i >= 3)
-	{
-		document.getElementById("nextBtn").setAttribute("disabled", "disabled");		
-	}
-}
-
-function setCard()
-{
-	var path = firebase.database().ref("Categories/" + topic + "/" + subtopic);
-	path = path.child("Card" + i);
-	var questionRef = path.child("Question");
-	var answerRef = path.child("Answer");
-	questionRef.once('value', function(snapshot)
-	{
-		var question = snapshot.val();
-		var div = document.getElementById("question");
-		var p = document.createElement("p");
-		p.innerHTML = question;
-		div.appendChild(p);
-	});
-	answerRef.once('value', function(snapshot)
-	{
-		var question = snapshot.val();
-		var div = document.getElementById("answer");
-		var p = document.createElement("p");
-		p.innerHTML = question;
-		div.appendChild(p);
-	});
-}
-
-function clearCard()
-{
 	var divQuestion = document.getElementById("question");
 	var divAnswer = document.getElementById("answer");
 
+	clearCard(divQuestion, divAnswer);
+	setCard();
+
+	document.getElementById("nextBtn").removeAttribute("disabled"); // Enable the "Next" button
+
+	// Check if the location of the card index is at the first card
+	if (intIndex <= 1) // Index is at the first card
+	{
+		document.getElementById("prevBtn").setAttribute("disabled", "disabled"); // Disable the "Previous" button	
+	}
+}
+
+// Go foward one card
+function next()
+{
+	intIndex++;
+
+	var divQuestion = document.getElementById("question");
+	var divAnswer = document.getElementById("answer");
+
+	clearCard(divQuestion, divAnswer);
+	setCard();
+
+	document.getElementById("prevBtn").removeAttribute("disabled"); // Enable the "Previous" button
+
+	// Check if the location of the card index is higher than the total card amount
+	if (intIndex >= intLastIndex) // Index is at the last card
+	{
+		document.getElementById("nextBtn").setAttribute("disabled", "disabled"); // Disable the "Next" button	
+	}
+}
+
+// Clear all values inside both divs
+function clearCard(divQuestion, divAnswer)
+{
 	while (divQuestion.firstChild)
 	{
 		divQuestion.removeChild(divQuestion.firstChild);
@@ -72,6 +63,51 @@ function clearCard()
 	}
 }
 
+// Set the card's question and answer values
+function setCard()
+{
+	var path = firebase.database().ref(strPath);
+
+	// Can be placed outside as a global variable with additional code?
+	path.once('value', function(snapshot) 
+		{ 
+			intLastIndex = snapshot.numChildren(); // Count all children nodes
+
+			// Check to see if there is only one card 
+			if (intLastIndex == 1)  // There is only once card found
+			{
+				document.getElementById("nextBtn").setAttribute("disabled", "disabled"); // Disable the "Next" button
+			}
+		}
+	);
+
+	path = path.child("Card" + intIndex);
+	var questionRef = path.child("Question");
+	var answerRef = path.child("Answer");
+
+	// Set the question value onto the card
+	questionRef.once('value', function(snapshot)
+	{
+		var question = snapshot.val();
+		var div = document.getElementById("question");
+		var p = document.createElement("p");
+		p.innerHTML = question;
+		div.appendChild(p);
+	});
+
+	// Set the answer value onto the card
+	answerRef.once('value', function(snapshot)
+	{
+		var answer = snapshot.val();
+		var div = document.getElementById("answer");
+		var p = document.createElement("p");
+		p.innerHTML = answer;
+		div.appendChild(p);
+	});
+} 
+
+/* Find container of the card and add/remove the class "hover"
+Using JQuery*/
 function rotateCard(btn)
 {
     var $card = $(btn).closest('.card-container');
--- js/setCategories.js
+++ js/setCategories.js
@@ -1,64 +1,95 @@
+var strPath = "Categories"; // Initialize location string for firebase database reference
+
+// Initialize the back button and category buttons
 window.onload = function() 
 {
 	document.getElementById('backBtn').onclick = resetCategories;
-	setCategories();
+	initiateCategories();
 }
 
-function setCategories() 
+// Set element attributes and call setCategories function
+function initiateCategories() 
 {
 	document.getElementById("categoryTitle").innerHTML = "Select Category";
 	document.getElementById("backBtn").setAttribute("disabled", "disabled");
-	firebase.database().ref("Categories").on("child_added", function(snapshot)
-	{
-		console.log(snapshot.key);
-		var topic = snapshot.key;
+
+	setCategories(strPath);
+}
+
+// Remove parent categories and initialize sub categories based on the subtopic value passed 
+function initiateSubCategories(subtopic) 
+{
+	document.getElementById('backBtn').removeAttribute("disabled");
+	document.getElementById('categoryTitle').innerHTML = subtopic;
+	var div = document.getElementById('categoryContainer');
+	strPath += "/" + subtopic; // Updated location string with subtopic
+
+	clearButtons(div);
+	setCategories(strPath);
+}
+
+// Initialize sub categories based on the string path (firebase database location)
+function setCategories(path)
+{
+	firebase.database().ref(path).on("child_added", function(snapshot)  // Reference all child nodes 
+	{	
+		var subtopic = snapshot.key;
 		var div = document.getElementById("categoryContainer");
 		var a = document.createElement("a");
 		a.className = "btn btn-info";
-		a.onclick = function() 
+		
+		// Set the onclick path to either reiterate sub categories or open the flashcard webpage
+		a.onclick = function()
 		{
-			setSubCategories(topic);
-		};
-		a.innerHTML = topic;
-		div.appendChild(a);
-	});
-}
+			if (isLastCategory(path + "/" + subtopic)) // Subtopic is the last parent node category
+			{
+				window.location.href = "flashcard.php";
+				sessionStorage.setItem("path", strPath + "/" + subtopic); 
+			}
+			else // Subtopic is not the last parent category
+			{
+				initiateSubCategories(subtopic);
+			}
+		}
 
-function setSubCategories(topic) {
-	document.getElementById('backBtn').removeAttribute("disabled");
-	var div = document.getElementById('categoryContainer');
-	clearButtons(div);
-	console.log(topic);
-	document.getElementById('categoryTitle').innerHTML = topic;
-	firebase.database().ref("Categories/" + topic).on("child_added", function(snapshot) 
-	{
-		console.log(snapshot.key);
-		var subtopic = snapshot.key;
-		var a = document.createElement('a');
-		a.className = "btn btn-info";
-		a.onclick = function() 
-		{
-			window.location.href = "flashcard.html";
-			sessionStorage.setItem("topic", topic);
-			sessionStorage.setItem("subtopic", subtopic);
-		};
 		a.innerHTML = subtopic;
 		div.appendChild(a);
 	});
 }
 
+// Check if firebase location is the last parent category
+function isLastCategory(path)
+{
+	var hasCard; // Initialize boolean
+
+	firebase.database().ref(path).once("value", function(snapshot) // Reference snapshot
+	{	
+		hasCard = snapshot.hasChild("Card1");
+	});
+	return hasCard; // Return boolean value
+}
+
+// Removes all child elements in parameter div
 function clearButtons(div) 
 {
-	while (div.firstChild) 
+	while (div.firstChild) // Loops until there are no more child elements
 	{
 		div.removeChild(div.firstChild);
 	}
 }
 
+// Go back one level in firebase database location and initialize categories
 function resetCategories() 
 {
 	var div = document.getElementById("categoryContainer");
+	document.getElementById("categoryTitle").innerHTML = firebase.database().ref(strPath).parent.key;
+	strPath = strPath.substring(0, strPath.length - (firebase.database().ref(strPath).key.length) - 1); // Removes the last known subtopic from location string 
+
 	clearButtons(div);
-	document.getElementById("categoryTitle").innerHTML = "";
-	setCategories();
+	setCategories(strPath);
+	console.log(firebase.database().ref(strPath).key);
+	if (firebase.database().ref(strPath).key == "categories") 
+	{
+		document.getElementById("backBtn").setAttribute("disabled", "disabled");
+	};
 }
